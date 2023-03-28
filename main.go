67027ab2703eb64ef33f112bdeb84cc79c2f5ae9
yleaf <11785335+leavesster@users.noreply.github.com>
2023-03-28 15:23:07 +0800

    add address func

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 
 	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/client-go/informers"
@@ -28,12 +30,18 @@ func main() {
 	ingressInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			ingress := obj.(*networkingv1.Ingress)
-			fmt.Printf("Ingress %s/%s created\n", ingress.Namespace, ingress.Name)
+			fmt.Printf("add ingress: %s", ingressDescription(ingress))
 		},
 		UpdateFunc: func(oldObj, newObj interface{}) {
 			oldIngress := oldObj.(*networkingv1.Ingress)
+			oldAddress := getAddress(oldIngress)
 			newIngress := newObj.(*networkingv1.Ingress)
-			fmt.Printf("Ingress %s/%s updated from %+v to %+v\n", newIngress.Namespace, newIngress.Name, oldIngress.Spec, newIngress.Spec)
+			newAddress := getAddress(newIngress)
+
+			if oldAddress != newAddress {
+				fmt.Printf("Ingress %s/%s updated from %s to %s", newIngress.Namespace, newIngress.Name, oldAddress, newAddress)
+				updateDns(getHosts(newIngress), newAddress)
+			}
 		},
 		DeleteFunc: func(obj interface{}) {
 			ingress := obj.(*networkingv1.Ingress)
@@ -43,4 +51,64 @@ func main() {
 
 	informerFactory.Start(nil)
 	informerFactory.WaitForCacheSync(nil)
+}
+
+func getHosts(ingress *networkingv1.Ingress) []string {
+	hosts := []string{}
+	tls := ingress.Spec.TLS
+	if len(tls) > 0 {
+		for _, tls := range tls {
+			hosts = append(hosts, tls.Hosts...)
+		}
+		return hosts
+	}
+	for _, rule := range ingress.Spec.Rules {
+		hosts = append(hosts, rule.Host)
+	}
+
+	return hosts
+}
+
+func getAddress(ingress *networkingv1.Ingress) string {
+	if ingress.Status.LoadBalancer.Ingress != nil {
+		for _, lbIngress := range ingress.Status.LoadBalancer.Ingress {
+			if lbIngress.Hostname != "" {
+				return lbIngress.Hostname
+			}
+			if lbIngress.IP != "" {
+				return lbIngress.IP
+			}
+		}
+	}
+
+	return ""
+}
+
+func ingressDescription(ingress *networkingv1.Ingress) string {
+	json, err := json.Marshal(ingress)
+	if err != nil {
+		return ""
+	}
+
+	return string(json)
+}
+
+func updateDns(hosts []string, address string) {
+	// if address is IP, update DNS A record
+	if net.ParseIP(address) != nil {
+		for _, host := range hosts {
+			// update DNS A record
+			fmt.Printf("update DNS A record for %s to %s", host, address)
+		}
+		return
+	}
+
+	// if address is hostname, update DNS CNAME record
+	if _, err := net.LookupHost(address); err != nil {
+		for _, host := range hosts {
+			// update DNS CNAME record
+			fmt.Printf("update DNS CNAME record for %s to %s", host, address)
+		}
+		return
+	}
 }
